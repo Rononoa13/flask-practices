7e9a13204eef589419c6f32d62fd87c0b60d4f7f
Rononoa13 <sumitluitel42@gmail.com>
2022-12-16 10:32:20 +0545

    #5 Flask WTF

--- codemy-blog/app.py
+++ codemy-blog/app.py
@@ -1,7 +1,49 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
+
+# Create a FORM Class
+class NameForm(FlaskForm):
+    name = StringField("Enter your name", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    #  DateField
+	# DateTimeField
+	# DecimalField
+	# FileField
+	# HiddenField
+	# MultipleField
+	# FieldList
+	# FloatField
+	# FormField
+	# IntegerField
+	# PasswordField
+	# RadioField
+	# SelectField
+	# SelectMultipleField
+	# SubmitField
+	# StringField
+	# TextAreaField
+
+	## Validators
+	# DataRequired
+	# Email
+	# EqualTo
+	# InputRequired
+	# IPAddress
+	# Length
+	# MacAddress
+	# NumberRange
+	# Optional
+	# Regexp
+	# URL
+	# UUID
+	# AnyOf
+	# NoneOf
 
 '''
 Search filters in jinja website.
@@ -32,9 +74,19 @@ def index():
 def user(name):
     return render_template("user.html", user_name=name)
 
+# Create Name page
+@app.route("/name", methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('nameForm.html', name=name, form=form)
+
+
 #  Create custom Error pages
-
-
 # Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
@@ -45,3 +97,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error(e):
     return render_template("500.html"), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Create a FORM Class
